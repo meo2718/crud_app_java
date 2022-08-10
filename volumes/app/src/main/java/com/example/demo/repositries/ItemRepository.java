b68f91764aa537f020e2a0303637fcf9c1c4de3b
meo2718 <agv1291.@gmail.com>
2022-08-10 19:34:40 +0900

    fix: formとentityの分割 #1

--- volumes/app/src/main/java/com/example/demo/repositries/ItemRepository.java
+++ volumes/app/src/main/java/com/example/demo/repositries/ItemRepository.java
@@ -6,15 +6,16 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.demo.entities.ItemEntity;
 //import com.example.demo.models.InquiryForm;
 //Kanato0331
 //import com.example.demo.models.InquiryForm2;
-import com.example.demo.models.ItemFormOld;
+//import com.example.demo.models.ItemFormOld;
 
 	@Repository
-	public interface ItemRepository extends JpaRepository<ItemFormOld, Long>{
-		Optional<ItemFormOld> findById(Long id);
-		List<ItemFormOld> findAll();
+	public interface ItemRepository extends JpaRepository<ItemEntity, Long>{
+		Optional<ItemEntity> findById(Long id);
+		List<ItemEntity> findAll();
 		
 		
 	}
